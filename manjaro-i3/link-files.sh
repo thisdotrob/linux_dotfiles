062d37cf3e6f377178b8239f21cd9df38a9f28b2
thisdotrob <rs@gower.st>
2018-06-12 18:42:02 +0100

    Update settings for HiDPI

---
+++ manjaro-i3/link-files.sh
@@ -0,0 +1,9 @@
+files to link:
+
+/home/rs/.lightdm-startup
+/etc/lightdm/lightdm.conf
+/etc/lightdm/lightdm-gtk-greeter.conf
+/home/rs/.config/dunst/dunstrc
+/home/rs/.i3/config
+/home/rs/.Xresources
+/home/rs/.profile
