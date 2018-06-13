4c032b355be9f216ede5e89b13f9ac9937ba02d1
thisdotrob <rs@gower.st>
2018-06-13 10:44:42 +0100

    Add config file for intel-media-driver

---
+++ manjaro-i3/etc/profile.d/intel-media.sh
@@ -0,0 +1,4 @@
+# add libva driver path/name exporting for intel media solution
+# uncomment the LIBVA_DRIVER_NAME line to use the Intel Media Driver (iHD) for VAAPI
+export LIBVA_DRIVERS_PATH=/usr/lib/dri
+export LIBVA_DRIVER_NAME=iHD
